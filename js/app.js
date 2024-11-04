e3aae22b913b4f3534dfe9a8957f31f9beb52ffc
samira-n <s.nabievaaa@yandex.ru>
2024-11-04 23:52:17 +0300

    create pop up form

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// Открытие попапа
+document.getElementById("openPopUpBtn").addEventListener("click", function(){
+    document.getElementById("popUp").classList.add("open");
+})
+
+// Закрытие попапа при нажатии на клавишу Escape
+window.addEventListener("keydown", (e) => {
+    if(e.key === "Escape") {
+        document.getElementById("popUp").classList.remove("open");
+    }
+})
+
+// Предотвращение закрытия при клике внутри попапа
+document.querySelector(".popUpBox").addEventListener("click", (event) => {
+    event.stopPropagation();
+});
+
+// Закрытие попапа при клике снаружи
+document.getElementById("popUp").addEventListener("click", (event) => {
+    event.currentTarget.classList.remove("open");
+});
+
+
+document.getElementById("reserve").addEventListener("click", function(){
+    const form = document.querySelector("form");
+    const inputs = document.querySelectorAll("input", "textarea");
+    let isFormValid = true;
+
+    inputs.forEach(input => {
+        if(!input.value.trim()){
+            isFormValid = false;
+        }
+    });
+
+    if(isFormValid){
+        document.getElementById("popUp").classList.remove("open");
+    }
+});
